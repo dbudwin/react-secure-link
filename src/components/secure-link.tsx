aced8b410ff39a7632c595b2871fb5230d305048
Drew Budwin <drew@budw.in>
2021-05-25 21:15:19 -0400

    Fix bug where the text 0 was being treated as null
    See issue #11

--- src/components/secure-link.tsx
+++ src/components/secure-link.tsx
@@ -3,13 +3,21 @@ import React, { ReactElement } from "react";
 type SecureLinkProps = JSX.IntrinsicElements["a"]
 
 export function SecureLink({ ...props }: SecureLinkProps): ReactElement {
+    let element: React.ReactNode;
+
+    if (props.children === null || props.children === undefined || props.children === "") {
+        element = props.href;
+    } else {
+        element = props.children;
+    }
+
     return (
         <a
             target="_blank"
             rel="noopener noreferrer"
             {...props}
         >
-            {props.children ? props.children : props.href}
+            {element}
         </a>
     );
 }
